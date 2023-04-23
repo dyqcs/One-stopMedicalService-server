29bec75381b49268d8fe3c1fb38076291268e304
dyqcs <3130119880@qq.com>
2023-04-23 11:31:01 +0800

    redis改成本地运行

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 本地swagger地址:
-http://localhost:8081/swagger-ui/index.html
+http://8.130.37.22:8081/swagger-ui/index.html
 
 注意：redis本地启动，远程服务器带不动
--- src/test/java/com/example/onestopmedicalserviceserver/OneStopMedicalServiceServerApplicationTests.java
+++ src/test/java/com/example/onestopmedicalserviceserver/OneStopMedicalServiceServerApplicationTests.java
@@ -10,12 +10,12 @@ import java.util.Date;
 @SpringBootTest
 class OneStopMedicalServiceServerApplicationTests {
 
-    @Test
-    void contextLoads() throws ParseException {
-        String str= "2022-12-28";
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        Date date = sdf.parse(str);
-        System.out.println(date);
-    }
+//    @Test
+//    void contextLoads() throws ParseException {
+//        String str= "2022-12-28";
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//        Date date = sdf.parse(str);
+//        System.out.println(date);
+//    }
 
 }
